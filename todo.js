22dba79663d200f32c1c247dff5c9cd64230874a
Vincent030299 <vincentschool0302@hotmail.com>
2020-06-12 04:48:36 +0200

    fixed some bugs

--- todo.js
+++ todo.js
@@ -1,14 +1,18 @@
 if (getCookie("username") == loginDetails[0].user) {
     let name = document.querySelector("#name");
     let tag = document.querySelector("#tag");
-    let toDos = [];
     // create a new cookie
     let createTagFunction = () => {
+        let toDos = JSON.parse(getCookie("todo"));
+        if (toDos == undefined) {
+            toDos = [];
+        }
         toDos[toDos.length] = {
             name: name.value,
             tag: tag.value
         };
         setCookie('todo', JSON.stringify(toDos));
+        location.href = "todo.html";
     }
     let createTag = document.querySelector("#create").addEventListener("click", createTagFunction);
     
